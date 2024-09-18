a6a492aaf3acb43afe412a3c9f7f871f07d38eca
ruben montani <ruben_montani@hotmail.com>
2024-09-18 18:43:21 -0300

    tabla2,base

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-import {App} from './formApp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UsuariosComponents } from './usuariosComponents';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
+  
     <UsuariosComponents/>
   </>
 );
